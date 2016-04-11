78b1fb2c17c2e098ae01c4bdae9dc7675b3f4c89
Darrel Kathan <darrel.kathan@grainger.com>
2016-04-11 17:13:10 -0500

    updated testpwd

--- test/DUCtrl.js
+++ test/DUCtrl.js
@@ -3,22 +3,36 @@ var DUApp = angular.module("DUApp",[]);
 DUApp.controller('DUCtrl', function ($scope, $http, $rootScope){
   var du = DropUpload({targetElement: '#du', hoverClass: 'my-class'});
   
+  $scope.upload = function(){
+    /*************************************************
+     * Change the value of my_url to set it in action
+     *************************************************/
+     var my_url = 'http://your-server.url/that/receives/uploads'
+    du.upload(my_url, function(e) {
+      var i = 0;
+    });
+  }
+  
   du.addEventListener('files-dropped', function(e) {
+    /************************************************
+     * e.detail contains an array of the files that were dropped.
+     *************************************************/
     $scope.files = e.detail;
     $scope.$apply();
   });
   
   du.addEventListener('progress', function(e) {
+    /*************************************************
+     * Each file has a progress property that is updated,
+     * which is being used in the Angular template to set 
+     * the width of the progress bar.
+     * Since the list of files is in $scope.files,
+     * all that needs to be done is to call $scope.$apply.
+     **************************************************/
     $scope.$apply();
   });
   
   du.addEventListener('error', function(e) {
     $('#debug').text('Some bad stuff happened');
   });
-  
-  $scope.upload = function(){
-    du.upload('http://your-server.url/that/receives/uploads', function(e) {
-      var i = 0;
-    });
-  }
 });
